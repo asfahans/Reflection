704982558b47852c99f4eedfe3f2526c06b90b99
Asfahan Shaikh <asfahan.shaikh@gmail.com>
2021-07-23 17:15:42 +0530

    Improved Intro Page

---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*==================== Intro Parallax ====================*/
+
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".rock", 10, { y: -300 })
+  .to(".girl", 10, { y: -200 }, "-=10")
+  .fromTo(".bg1", { y: -35 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "0%" }, "-=10")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "300%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
+
+/*==================== SHOW SCROLL UP ====================*/
+
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
